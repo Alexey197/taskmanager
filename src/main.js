1cb0d46201965103aba55c8dafafbe62e721cb9d
Alexey197 <margadonnn3@gmail.com>
2022-08-09 08:41:36 +0300

    task changes fish/mock and add mock/task main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import{createSiteMenuTemplate} from './components/site-menu'
 import{createTaskTemplate} from './components/task'
 import{createTaskEditTemplate} from './components/task-edit'
 import {generateFilters} from "./mock/filter"
+import {generateTasks} from "./mock/task"
 
 const TASK_COUNT = 22
 
@@ -21,14 +22,18 @@ const filters = generateFilters()
 render(siteMainElement, createFilterTemplate(filters))
 render(siteMainElement, createBoardTemplate())
 
+const tasks = generateTasks(TASK_COUNT)
 const taskListElement = siteMainElement.querySelector(`.board__tasks`)
-render(taskListElement, createTaskEditTemplate())
+render(taskListElement, createTaskEditTemplate(tasks[0]))
 
-new Array(TASK_COUNT)
-    .fill(``)
-    .forEach(
-        () => render(taskListElement, createTaskTemplate())
-    )
+tasks.slice(1).forEach((task) => render(taskListElement, createTaskTemplate(task)))
+// render(taskListElement, createTaskEditTemplate())
+//
+// new Array(TASK_COUNT)
+//     .fill(``)
+//     .forEach(
+//         () => render(taskListElement, createTaskTemplate())
+//     )
 
 const boardElement = siteMainElement.querySelector(`.board`)
 render(boardElement, createLoadMoreButtonTemplate())
