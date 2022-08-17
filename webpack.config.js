2662532f53e6bb8c3253e57efeb4a076d97d204f
Alexey197 <margadonnn3@gmail.com>
2022-08-17 21:28:19 +0300

    flatpickr install package.json update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`)
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`)
 
 module.exports = {
   entry: './src/main.js',
@@ -10,5 +11,16 @@ module.exports = {
   devtool: 'source-map',
   devServer: {
     port: 3000
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new MomentLocalesPlugin()
+  ]
 }
